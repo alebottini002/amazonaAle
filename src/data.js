02bad06cb02b7c249b761786baa0a7001ccece20
Ale Bottini <alebottini002@gmail.com>
2021-02-01 20:24:59 -0300

    creating react-app

---
+++ src/data.js
@@ -0,0 +1,16 @@
+const data = {
+    products: [
+        {
+            _id: "1",
+            name: "nike remera",
+            category:"remeras",
+            image:"/images/p1.jpg",
+            price: 2500,
+            brand:"nike",
+            rating: 4.5,
+            numReviews: 10,
+            description: "Remera alta calidad",
+
+        },
+    ],
+};
